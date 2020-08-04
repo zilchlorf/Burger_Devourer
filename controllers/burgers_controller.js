9e93f32a01421b0d2291ae91b6ee3e33782fd4d0
Max Edmonds <maxedmonds05@gmail.com>
2020-08-03 17:58:16 -0700

    modify connection for heroku app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,6 @@
 var express = require("express");
 
 var router = express.Router();
-
-// Import the model (cat.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
